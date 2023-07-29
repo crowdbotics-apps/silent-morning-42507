ab143d6fe525a49e38780d32292014722798b65a
Crowdbotics <team@crowdbotics.com>
2023-07-29 11:31:52 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "silent-morning-42507",
+  url: "https://silent-morning-42507.botics.co",
+  api: "https://silent-morning-42507.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
